c1fa99bce7a1bfed23c60819345952b6091c6d72
S. Hutchins <ms.s.hutchins@gmail.com>
2018-08-23 18:54:59 -0500

    Added shell functions for shinyconfig.

---
+++ R/shinyconfig.R
@@ -0,0 +1,9 @@
+#' shinyconfig: A package for computating the notorious bar statistic.
+#'
+#' The shinyconfig package provides functions to aid in the configuration and setup for shiny apps as standalone apps or packages.
+#'
+#' @section shinyconfig functions:
+#'
+#' @docType package
+#' @name shinyconfig
+NULL
